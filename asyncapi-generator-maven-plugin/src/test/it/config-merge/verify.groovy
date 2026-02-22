d4556e64cf55dcc081503e35e5f7e4785e681c42
Salvador Bascunan <salvador.bascunan@tietoevry.com>
2026-02-22 15:23:36 +0100

    added one more 'it' test to show structure for future usage

--- asyncapi-generator-maven-plugin/src/test/it/config-merge/verify.groovy
+++ asyncapi-generator-maven-plugin/src/test/it/config-merge/verify.groovy
@@ -1,10 +1,14 @@
 def base = new File(basedir, "target/generated-sources/asyncapi")
+
+// define where the generated files should be for each contract, based on the configuration in the pom.xml
 def aClientDir = new File(base, "com/example/a/client")
 def aSchemaDir = new File(base, "com/example/a/schema")
 def bClientDir = new File(base, "com/example/b/client")
 def bSchemaDir = new File(base, "com/example/b/schema")
 def aModelDir = new File(base, "com/example/a/model")
 def bModelDir = new File(base, "com/example/b/model")
+
+// assert that the expected directories exist or do not exist based on the configuration in the pom.xml
 assert aModelDir.exists() : "Expected model directory for contract A"
 assert bModelDir.exists() : "Expected model directory for contract B"
 assert aClientDir.exists() : "Expected client directory for contract A (global client.type)"
