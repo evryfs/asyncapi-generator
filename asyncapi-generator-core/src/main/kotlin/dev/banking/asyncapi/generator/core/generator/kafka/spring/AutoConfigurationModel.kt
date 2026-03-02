f657ef589c0535f5f751e88d9976709934c48046
Salvador Bascunan <salvador.bascunan@tietoevry.com>
2026-03-02 08:41:28 +0100

    added AutoConfigurationModel and ResourceGenerator

---
+++ asyncapi-generator-core/src/main/kotlin/dev/banking/asyncapi/generator/core/generator/kafka/spring/AutoConfigurationModel.kt
@@ -0,0 +1,7 @@
+package dev.banking.asyncapi.generator.core.generator.kafka.spring
+
+data class AutoConfigurationModel(
+    val packageName: String,
+    val className: String,
+    val clientPackage: String,
+)
