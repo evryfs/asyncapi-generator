014545bb19eecf304730d0b868724f4db81ba4c8
Salvador Bascunan <salvador.bascunan@tietoevry.com>
2026-03-16 22:13:59 +0100

    added ReferenceCategoryKey to identify what parser and validator needs to run

---
+++ asyncapi-generator-core/src/main/kotlin/dev/banking/asyncapi/generator/core/model/references/ReferenceCategoryKey.kt
@@ -0,0 +1,18 @@
+package dev.banking.asyncapi.generator.core.model.references
+
+enum class ReferenceCategoryKey {
+    SCHEMA,
+    CHANNEL,
+    MESSAGE,
+    OPERATION,
+    SERVER,
+    PARAMETER,
+    SECURITY_SCHEME,
+    MESSAGE_TRAIT,
+    OPERATION_TRAIT,
+    CORRELATION_ID,
+    EXTERNAL_DOC,
+    TAG,
+    BINDING,
+    COMPONENT,
+}
